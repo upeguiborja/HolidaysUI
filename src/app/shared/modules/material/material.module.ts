26d8c4b8a0aa5276c24cbf40498d8d1a6399323e
Mateo Upegui Borja <upeguiborja@gmail.com>
2024-06-02 18:05:40 -0500

    feat: mejoras de diseño

--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
@@ -18,6 +21,12 @@ import { MatIconModule } from '@angular/material/icon';
     MatToolbarModule,
     MatDatepickerModule,
   ],
-  providers: [provideNativeDateAdapter()],
+  providers: [
+    provideNativeDateAdapter(),
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' },
+    },
+  ],
 })
 export class MaterialModule {}
