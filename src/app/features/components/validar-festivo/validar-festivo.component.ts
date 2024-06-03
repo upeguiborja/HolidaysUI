cc8fe31340d8dd884bca51d3138abf2e8080564e
Mateo Upegui Borja <upeguiborja@gmail.com>
2024-06-02 21:32:05 -0500

    feat: mejoras gráficas

--- src/app/features/components/validar-festivo/validar-festivo.component.ts
+++ src/app/features/components/validar-festivo/validar-festivo.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MaterialModule } from '../../../shared/modules/material/material.module';
 import { FormControl } from '@angular/forms';
 import { FestivosService } from '../../services/festivos.service';
-import { interval, throttle } from 'rxjs';
+import { delay } from 'rxjs';
 
 type Status = 'LOADING' | 'LOADED' | 'ERROR';
 
@@ -27,16 +27,19 @@ export class ValidarFestivoComponent {
   public onVerify() {
     this.status = 'LOADING';
     if (!this.date.value) return;
-    this.festivosService.verificarFestivo(this.date.value).subscribe({
-      next: (isHoliday) => {
-        this.isHoliday = isHoliday;
-        this.status = 'LOADED';
-      },
-      error: (err) => {
-        console.error(err);
-        this.status = 'ERROR';
-      },
-    });
+    this.festivosService
+      .verificarFestivo(this.date.value)
+      .pipe(delay(500)) // Simulamos un delay de 500ms para que se vea el spinner.
+      .subscribe({
+        next: (isHoliday) => {
+          this.isHoliday = isHoliday;
+          this.status = 'LOADED';
+        },
+        error: (err) => {
+          console.error(err);
+          this.status = 'ERROR';
+        },
+      });
   }
 
   public get formattedDate() {
